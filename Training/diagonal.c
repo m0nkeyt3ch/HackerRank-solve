9e064ed713453f7440a8ee59eb9bea211e4dcb02
Michael Octavianus Korintus <michael.201096@gmail.com>
2019-04-19 12:15:22 +0800

    update for diagonal.c

--- Training/diagonal.c
+++ Training/diagonal.c
@@ -1,11 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #define MAX 500
 
 int main(int argc, char const *argv[]) {
   int input, i, j;
   int array[MAX][MAX];
+  int diagon1, diagon2;
 
-  // printf("%d\n", );
+  scanf("%d", &input);
+  for (i = 0; i < input; i++) {
+    for (j = 0; j < input; j++) {
+      scanf("%d", &array[i][j]);
+    }
+  }
+
+  for (i = 0; i < input; i++) {
+    for (j = 0; j < input; j++) {
+      // diagon1 = array[i++][j++];
+    }
+  }
+  printf("%d\n", diagon1);
 
   return 0;
 }
