e84e853d59e97a8faf152328e900eee078c40a13
Script Kiddie <michael.201096@gmail.com>
2019-04-19 14:50:46 +0800

    Update diagonal.c

--- Training/diagonal.c
+++ Training/diagonal.c
@@ -1,11 +1,13 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
 #define MAX 500
 
 int main(int argc, char const *argv[]) {
   int input, i, j;
   int array[MAX][MAX];
-  int diagon1, diagon2;
+  int diagon1 = 0, diagon2 = 0;
 
   scanf("%d", &input);
   for (i = 0; i < input; i++) {
@@ -14,12 +16,21 @@ int main(int argc, char const *argv[]) {
     }
   }
 
-  for (i = 0; i < input; i++) {
-    for (j = 0; j < input; j++) {
-      // diagon1 = array[i++][j++];
-    }
+  i = 0;
+  while (i < input) {
+    diagon1 = diagon1 + array[i][i];
+    i++;
   }
-  printf("%d\n", diagon1);
+
+  i = 0;
+  j = input - 1;
+  while (i < input) {
+    diagon2 = diagon2 + array[i][j];
+    i++;
+    j--;
+  }
+
+  printf("%d\n", abs(diagon1 - diagon2));
 
   return 0;
 }
