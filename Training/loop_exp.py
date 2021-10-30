3739c739c7bd04111d730d36c64458f69b53511b
Michael <monkeytech34@gmail.com>
2021-10-30 12:45:58 +0700

    added new file

---
+++ Training/loop_exp.py
@@ -0,0 +1,8 @@
+n = int(input())
+total  = 0
+i = 1
+while (i < n):
+    total = total + (2*i+1)
+    i = i + 1
+
+print('Total = ' + str(total))
---
+++ Training/stack.py
@@ -0,0 +1,17 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+    
+    def push(self, item):
+        self.item.appen(item)
+
+    def pop(self):
+        return self.items.pop()
+    
+    def peek(self):
+        return self.items
+    
+    def is_empty(self):
+        return self.items == []
+
+s = stack()
